5a52e55f29298a7d6a2adfa776d4e764c2c36a2c
Eunchul Jung <agfe09@gmail.com>
2010-12-06 15:57:53 +0900

    create reservation model

---
+++ linked/db/migrate/20101206065344_create_reservations.rb
@@ -0,0 +1,20 @@
+class CreateReservations < ActiveRecord::Migration
+  def self.up
+    create_table :reservations do |t|
+      t.integer :coupon_id
+
+      t.string :height
+      t.string :shoe_size
+
+      t.string :resort
+
+      t.datetime :used_at
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :reservations
+  end
+end
