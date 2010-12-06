6279a64e5c21abae1050ed83523093ea6c1b1336
Eunchul Jung <agfe09@gmail.com>
2010-12-06 17:02:38 +0900

    create admin model

---
+++ linked/app/controllers/admin/sessions_controller.rb
@@ -0,0 +1,27 @@
+class Admin::SessionsController < Devise::SessionsController
+
+  before_filter :authenticate_admin!, :only => [:destroy]
+
+  include Devise::Controllers::InternalHelpers
+
+  # GET /resource/sign_in
+  def new
+    clean_up_passwords(build_resource)
+    render_with_scope :new
+  end
+
+  # POST /resource/sign_in
+  def create
+    logger.debug("-------------------------hello")
+    resource = warden.authenticate!(:scope => resource_name, :recall => "new")
+    set_flash_message :notice, :signed_in
+    #sign_in_and_redirect(resource_name, resource)
+  end
+
+  # GET /resource/sign_out
+  def destroy
+    set_flash_message :notice, :signed_out if signed_in?(resource_name)
+    sign_out_and_redirect(resource_name)
+  end
+
+end
