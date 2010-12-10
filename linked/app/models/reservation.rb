6203b5ef9f09a451289ca6e88a6ba8af2bbf208f
Eunchul Jung <agfe09@gmail.com>
2010-12-10 18:04:31 +0900

    update: users pages & reservation's validations

--- linked/app/models/reservation.rb
+++ linked/app/models/reservation.rb
@@ -2,4 +2,16 @@ class Reservation < ActiveRecord::Base
 
   belongs_to :coupon, :counter_cache => true
 
+  validates_presence_of :user_name, :resort, :height, :shoe_size
+
+  class CouponLimitQuantityValidator < ActiveModel::EachValidator
+    def validate_each(record, attribute, value)
+      if record.coupon.quantity.to_i < record.coupon.reservations_count.to_i + 1
+        record.errors[attribute] << "Quantity exceeded"
+      end
+    end
+  end
+
+  validates :coupon_id, :coupon_limit_quantity => true, :on => :create
+
 end
--- linked/test/unit/reservation_test.rb
+++ linked/test/unit/reservation_test.rb
@@ -1,8 +1,18 @@
 require 'test_helper'
 
 class ReservationTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  test "should raised exceeded coupon's quantity" do
+    c = coupons(:one)
+    reservation = Reservation.new(:coupon_id => c.id)
+    reservation.user_name = "foo"
+    reservation.resort = "bar resort"
+    reservation.height = "90"
+    reservation.shoe_size = "30"
+
+    assert_raise ActiveRecord::RecordInvalid do
+      reservation.save!
+    end
   end
+
 end
