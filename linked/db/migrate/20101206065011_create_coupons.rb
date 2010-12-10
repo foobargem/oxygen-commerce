0814b7ce8c6755c8eb416501fae7b2a3e753fa47
Eunchul Jung <agfe09@gmail.com>
2010-12-10 16:54:06 +0900

    update db schema

--- linked/db/migrate/20101206065011_create_coupons.rb
+++ linked/db/migrate/20101206065011_create_coupons.rb
@@ -6,13 +6,16 @@ class CreateCoupons < ActiveRecord::Migration
 
       t.string :coupon_number
 
+      # 구매수량/사용한 수량
+      t.integer :quantity, :default => 0
+      t.integer :reservations_count, :default => 0
+
       # 구매자
       t.string :purchaser_name
+      t.string :phone_number
 
       # 구매업체(중계업체)
       t.string :agency_name
-
-      t.string :phone_number
 
       t.timestamps
     end
--- linked/db/migrate/20101206065344_create_reservations.rb
+++ linked/db/migrate/20101206065344_create_reservations.rb
@@ -2,11 +2,16 @@ class CreateReservations < ActiveRecord::Migration
   def self.up
     create_table :reservations do |t|
       t.integer :coupon_id
+      t.integer :product_id
 
+      t.string :user_name
+      t.string :resort
+
+      # 장비
+      t.string :shoe_type
+      t.string :board_stance
       t.string :height
       t.string :shoe_size
-
-      t.string :resort
 
       t.datetime :used_at
       t.string :part_time
