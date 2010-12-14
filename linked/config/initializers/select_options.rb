ebe69d8f49db454b651ab64643b071ed959878b3
Eunchul Jung <agfe09@gmail.com>
2010-12-14 19:36:26 +0900

    update: select_options

--- linked/config/initializers/select_options.rb
+++ linked/config/initializers/select_options.rb
@@ -8,3 +8,19 @@ PART_TIME_OPTIONS = {
   "오전+오후" => "오전+오후",
   "오후+야간" => "오후+야간"
 }
+
+TICKET_TYPE_OPTIONS = {
+  "daily" => "개별권",
+  "season" => "시즌권",
+  "free" => "자유권"
+}
+
+RESORT_OPTIONS = %w{
+  하이원 용평 보광 대명 성우 지산
+}
+
+BOARD_STANCE_OPTIONS = %w{
+  레귤러(Regular)
+  구피(Goofy)
+}
+
