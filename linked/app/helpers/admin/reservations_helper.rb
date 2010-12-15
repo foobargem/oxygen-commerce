03b9080198958669a02b4715debbfeabc5bf7c1a
Eunchul Jung <agfe09@gmail.com>
2010-12-15 17:23:31 +0900

    update: admin page styles

--- linked/app/helpers/admin/reservations_helper.rb
+++ linked/app/helpers/admin/reservations_helper.rb
@@ -1,2 +1,15 @@
+# encoding: utf-8
 module Admin::ReservationsHelper
+
+  def display_group_by_string(group_by, key)
+    case group_by
+    when "agency"
+      "구매업체: #{key}"
+    when "provider"
+      "공급업체: #{key}"
+    else
+      "이용일: #{key}"
+    end
+  end
+
 end
